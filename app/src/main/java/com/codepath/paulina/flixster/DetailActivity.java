8ca0798eae35dae6ac58c64086da5b11aef1a5cd
Paulina Acosta <paulinaacostac@hotmail.com>
2021-09-11 22:14:34 -0400

    Finished required stories Unit 2

--- app/src/main/java/com/codepath/paulina/flixster/DetailActivity.java
+++ app/src/main/java/com/codepath/paulina/flixster/DetailActivity.java
@@ -3,18 +3,33 @@ package com.codepath.paulina.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.paulina.flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 
-public class DetailActivity extends AppCompatActivity {
+import okhttp3.Headers;
 
+public class DetailActivity extends YouTubeBaseActivity {
+
+    public static final String YOUTUBE_API_KEY="AIzaSyABCu5qToteCdizHUhTaZolseXVXhmWwOo";
+    public static final String YOUTUBE_API_KEY_VIDEO="AIzaSyAHpKACyrPHDXX1CXmkAhjnJLapTc_Z888";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     TextView tvTitle;
     TextView tvOverview;
     RatingBar ratingBar;
+    YouTubePlayerView youtubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,7 @@ public class DetailActivity extends AppCompatActivity {
         tvTitle = findViewById(R.id.tvTitle);
         tvOverview = findViewById(R.id.tvOverview);
         ratingBar = findViewById(R.id.ratingBar);
+        youtubePlayerView = findViewById(R.id.player);
 
         //String title = getIntent().getStringExtra("title"); --------> This is not needed anymore because the Parcels library is wrapping the whole movie object instead of attribute by attribute
 
@@ -32,5 +48,44 @@ public class DetailActivity extends AppCompatActivity {
         tvOverview.setText(movie.getOverview());
         ratingBar.setRating((float) movie.getRating());
 
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    if (results.length() == 0)
+                    {
+                        return;
+                    }
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity","the Youtube key is: "+youtubeKey);
+                    initializeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    Log.e("DetailActivity","Failed to parse JSON",e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+    }
+
+    private void initializeYoutube(String youtubeKey) {
+        youtubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity","onInitializationSuccess");
+                youTubePlayer.cueVideo(youtubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity","onInitializationFailure: "+youTubeInitializationResult.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/codepath/paulina/flixster/models/Movie.java
+++ app/src/main/java/com/codepath/paulina/flixster/models/Movie.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Parcel
 public class Movie
 {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
@@ -30,6 +31,7 @@ public class Movie
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException
@@ -61,5 +63,9 @@ public class Movie
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
